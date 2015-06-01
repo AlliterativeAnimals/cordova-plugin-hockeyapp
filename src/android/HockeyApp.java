0c179ac4b0553d35866687cf0ed347527800b502
David Godfrey <david@byng.co>
2015-06-01 12:58:33 +0100

    f

--- src/android/HockeyApp.java
+++ src/android/HockeyApp.java
@@ -38,10 +38,20 @@ public class HockeyApp extends CordovaPlugin {
         } else if(action.equals("versionStart")) {
             if (initialized) {
                 UpdateManager.register(cordova.getActivity(), token);
+                return true;
+            }
+            else {
+                callbackContext.error("cordova hockeyapp plugin not initialized, call start() first");
+                return false;
             }
         } else if (action.equals("versionStop")) {
             if (initialized) {
                 UpdateManager.unregister();
+                return true;
+            }
+            else {
+                callbackContext.error("cordova hockeyapp plugin not initialized, call start() first");
+                return false;
             }
         }
         else {
